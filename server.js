897e696dbc605b7a4a021beda96fdeedcd864119
Henry <henryolson@icloud.com>
2022-01-22 20:34:02 -0600

    test

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const path = require('path');
 const express = require('express');
-// const routes = require('./routes');
-// import sequelize connection
+//const routes = require('./routes');
+//import sequelize connection
 const sequelize = require('./config/connection');
 const axios = require('axios');
-const NodeGeocoder = require('node-geocoder');
 const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ defaultLayout: 'main' });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -20,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use(routes);
-
+    
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
