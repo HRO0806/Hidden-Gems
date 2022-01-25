910d573b0272b8b56b4d2570b6b8c9bd0bed4888
Henry <henryolson@icloud.com>
2022-01-25 16:34:33 -0600

    Built the post page as well as front-end logic for it

---
+++ public/javascript/post.js
@@ -0,0 +1,19 @@
+let button = document.querySelector(".post-button");
+
+function post(postedContent, postContents) {
+    postedContent.className = ".post";
+    postedContent.innerHTML = `<div class='post'> <p>${postContents}</p> </div>`;
+    document.body.append(postedContent);
+};
+
+button.addEventListener("click", function() {
+    let text = document.querySelector("#post").value;
+    let userPost = document.createElement("div");
+    
+    if(text === "") {
+        alert('Post must contain text');
+    }
+    else {
+        post(userPost, text);
+    }
+});
